580865edfefd3b68ae38a6f3626a7d09f0723534
Monica Gerard <mlgvla@gmail.com>
2022-09-13 13:23:23 -0400

    display tracks

--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
 
 function TracksPage() {
+  const baseUrl = "http://localhost:8001/tracks"
 
+  const [tracks, setTracks] = useState([])
+  
+  useEffect(() => {
+    fetch(baseUrl)
+    .then(res => res.json())
+    .then(tracks => setTracks(tracks))
+  }, [])
+  
     
   return (
     <div>
       <Search />
       <AddTrackForm />
-      <TracksList />
+      <TracksList tracks={tracks}/>
     </div>
   )
 }
